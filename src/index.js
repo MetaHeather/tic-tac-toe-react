4015e38aba3a6297f0000d72fd8daf4202294a52
Heather Etheridge <heathernetheridge@gmail.com>
2019-09-25 08:26:52 -0500

    game now checks for and prints winner

--- src/index.js
+++ src/index.js
@@ -1,59 +1,119 @@
-// Connect to the socket.io server
-var socket = io();  
-// listen to the server for the `add-circle` event
-socket.on('add-circle', function (data) {
-  // console.log(data);
-  addCircle(data);
-});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { arrayExpression } from '@babel/types';
 
 
-var circles = document.getElementById('circles');
-var initials = '';
-
-circles.addEventListener('click', function(evt) {
-  // replace current line of code with this code
-  socket.emit('add-circle', {
-    initials: initials,
-    x: evt.clientX,
-    y: evt.clientY,
-    dia: randomBetween(10,100),
-    rgba: getRandomRGBA()
-  });
-});
-
-
-document.getElementsByTagName('button')[0].addEventListener('click', function() {
-  circles.innerHTML = '';
-});
-
-do {
-  initials = getInitials();
-} while (initials.length < 2 || initials.length > 3);
-
-function getInitials() {
-  var input = prompt("Please enter your initials");
-  return input ? input.toUpperCase() : '';
-}
-
-function addCircle({x, y, dia, rgba, initials}) {
-  var el = document.createElement('div');
-  el.style.left = x - Math.floor(dia / 2 + 0.5) + 'px';
-  el.style.top = y - Math.floor(dia / 2 + 0.5) + 'px';
-  el.style.width = el.style.height = dia + 'px';
-  el.style.backgroundColor = rgba;
-  el.style.fontSize = Math.floor(dia / 3) + 'px';
-  el.style.color = 'white';
-  el.style.textAlign = 'center';
-  el.style.lineHeight = dia + 'px';
-  el.innerHTML = initials;
-  circles.appendChild(el);
-}
-
-function randomBetween(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function getRandomRGBA() {
-  return ['rgba(', randomBetween(0, 255), ',', randomBetween(0, 255), ',',
-    randomBetween(0, 255), ',', randomBetween(2, 10) / 10, ')'].join('');
-}
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+  
+  class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+        xIsNext: true,
+      };
+    }
+  
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+  
+    renderSquare(i) {
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
+    }
+  
+    render() {
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  
